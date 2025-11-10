baee449c3eca244701ac87949c6ae26f3519e742
Will White <60898498+Gh0strab@users.noreply.github.com>
2025-11-10 11:00:48 -0600

    Update WidgetDataManager.kt

--- app/src/main/java/com/flipcover/widgets/WidgetDataManager.kt
+++ app/src/main/java/com/flipcover/widgets/WidgetDataManager.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import android.graphics.Bitmap
 
 data class ChildWidgetData(
     val id: String,
